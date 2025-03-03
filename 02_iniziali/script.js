49d83d744fa7448e8972386006a2522c567c793f
AlbertoCadorin <alberto.cadori01@gmail.com>
2025-03-03 18:35:24 +0100

    feat: Write a function that takes an array of names and returns a new array with the initials of each word in the array provided.

--- 02_iniziali/script.js
+++ 02_iniziali/script.js
@@ -5,9 +5,19 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
 // Dichiara la funzione qui.
 
+function initialNames(name){
+    let initial = [];
+    for (let i = 0; i < name.length; i++){
+        if(name[i].length > 0 ){
+            initial.push(name[i][0]);
+        }
+    }
+    return initial;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
+console.log(initialNames(names));
 
 
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
